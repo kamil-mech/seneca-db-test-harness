f083d68e98809dd054856b9f4b00fdca0a66e240
Kamil Mech <kamil.mech@nearform.com>
2015-10-31 21:51:41 +0000

    complete cleanup

--- lib/kill-children.sh
+++ lib/kill-children.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
-
-docker kill $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
 echo "TOP PARENT: $1"
-ALL="$(pgrep -P $1)"
 
-if [[ "$ALL" != *"Usage"* ]]; then
-  for CHILD in ${ALL[@]}; do
-    echo "CHILD $CHILD"
-    SUB="$(pgrep -P $CHILD)"
-    for SUBCHILD in ${SUB[@]}; do
-      echo "SUBCHILD $SUBCHILD"
-      # kill $SUBCHILD
-      SUB_2="$(pgrep -P $SUBCHILD)"
-      for SUB_2_CHILD in ${SUB_2[@]}; do
-        echo "KILLING $SUB_2_CHILD"
-        kill $SUB_2_CHILD
-      done
-      kill $SUBCHILD
+# recursive
+function kill_children(){
+  echo "PROCESS: $1"
+  # check more
+  ALL="$(pgrep -P $1 || true)"
+
+  if [[ "$ALL" != "" ]]; then
+    for CHILD in ${ALL[@]}; do
+      kill_children "$CHILD"
+      # kill
+      echo "KILLING $CHILD"
+      kill "$CHILD" &> /dev/null || true
     done
-    kill $CHILD
-  done
-fi
+  fi
+  return 0
+}
+
+kill_children $1
+
+docker kill $(docker ps -a -q) &> /dev/null
+docker rm $(docker ps -a -q) &> /dev/null
+

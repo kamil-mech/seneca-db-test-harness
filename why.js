4d64e2a1001dfdf1dd3a872c57e393f22f42756f
Kamil Mech <kamil.mech@nearform.com>
2016-01-03 19:13:17 +0000

    add why.js for easier debugging

---
+++ why.js
@@ -0,0 +1,24 @@
+'use strict'
+
+var fs = require('fs')
+var _ = require('lodash')
+
+// takes in a string to look for in errors and reports the amount of occurences
+var target = process.argv[2]
+var results = {}
+
+var logfolder = __dirname + '/log/fail/' + target
+fs.stat(logfolder, function (err, res) {
+  if (err) return console.log(logfolder + ' folder not present - run sdbth')
+  
+  if (res.isDirectory()) {
+    var logs = fs.readdirSync(logfolder)
+    _.each(logs, function (log) {
+      if (log.indexOf('.err') > -1) {
+        console.log('\n' + log + '\n---\n')
+        console.log(fs.readFileSync(logfolder + '/' + log).toString())
+        console.log()
+      }
+    })
+  }
+})

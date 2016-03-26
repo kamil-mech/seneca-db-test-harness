ffd194bfd84f743a9cce409e0a2dff4430945ceb
kamil-mech <kamil.mech@nearform.com>
2016-03-26 17:14:25 +0000

    run mysql and psql in containers to avoid installation

--- dbs/init/init-mysql.sh
+++ dbs/init/init-mysql.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
 echo "INIT MYSQL"
-echo "CHECKING FOR mysql COMMAND"
-mysql --version
-if [[ "$?" -gt 0 ]]; then
-  echo "MISSING COMMAND mysql" 1>&2
-  echo "INSTALL VIA: sudo apt-get install mysql-client-core-5.6"  1>&2
-  echo  1>&2
-  exit 64
-fi
 
 USER="$1"
 PASSWORD="$2"
 NAME="$3"
 SCHEMA="$4"
 IP="$5"
+CID="$6"
 
 echo
 echo "USER: $USER"
@@ -22,15 +15,20 @@ echo "PASSWORD: $PASSWORD"
 echo "NAME: $NAME"
 echo "SCHEMA: $SCHEMA"
 echo "IP: $IP"
+echo "CID: $CID"
 echo
 
 echo "---"
 echo "INIT START"
-mysql -h$IP -u root -ppassword $NAME < $SCHEMA
-mysql -h$IP -u root -ppassword -e "GRANT ALL PRIVILEGES on *.* TO '"$USER"'@'%' identified by '"$PASSWORD"' WITH GRANT OPTION; FLUSH PRIVILEGES"
-mysql -h$IP -u root -ppassword -e "CREATE DATABASE test"
-mysql -h$IP -u root -ppassword -D "test" -e "CREATE TABLE sys_entity (base varchar(255) DEFAULT NULL, name varchar(255) DEFAULT NULL, zone varchar(255) DEFAULT NULL, fields blob, id varchar(255) NOT NULL, seneca blob) ENGINE=InnoDB DEFAULT CHARSET=latin1"
-mysql -h$IP -u root -ppassword -D "test" -e "CREATE TABLE test (id varchar (255) NOT NULL, test1 varchar (255), test2 blob, seneca blob) ENGINE=InnoDB DEFAULT CHARSET=latin1"
+while read chunk; do
+  chunk=$(echo "$chunk" | sed 's/`/\\\'\`'/g')
+  docker exec "$CID" bash -c "echo \"$chunk\" >> data.mysql.sql"  
+done < "$SCHEMA"
+docker exec "$CID" bash -c "mysql -h$IP -u root -ppassword $NAME < data.mysql.sql"
+docker exec "$CID" mysql -h"$IP" -u root -ppassword "$NAME" -e "GRANT ALL PRIVILEGES on *.* TO '"$USER"'@'%' identified by '"$PASSWORD"' WITH GRANT OPTION; FLUSH PRIVILEGES"
+docker exec "$CID" mysql -h"$IP" -u root -ppassword "$NAME" -e "CREATE DATABASE test"
+docker exec "$CID" mysql -h"$IP" -u root -ppassword -D "test" -e "CREATE TABLE sys_entity (base varchar(255) DEFAULT NULL, name varchar(255) DEFAULT NULL, zone varchar(255) DEFAULT NULL, fields blob, id varchar(255) NOT NULL, seneca blob) ENGINE=InnoDB DEFAULT CHARSET=latin1"
+docker exec "$CID" mysql -h"$IP" -u root -ppassword -D "test" -e "CREATE TABLE test (id varchar (255) NOT NULL, test1 varchar (255), test2 blob, seneca blob) ENGINE=InnoDB DEFAULT CHARSET=latin1"
 echo "INIT COMPLETE"
 echo "---"
 #mysql -u $USER -p$PASSWORD $NAME # used to log in
--- dbs/init/init-postgresql.sh
+++ dbs/init/init-postgresql.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
 echo "INIT POSTGRESQL"
-echo "CHECKING FOR psql COMMAND"
-psql --version
-if [[ "$?" -gt 0 ]]; then
-  echo "MISSING COMMAND psql" 1>&2
-  echo "INSTALL VIA: sudo apt-get install postgresql-client-9.4" 1>&2
-  echo 1>&2
-  exit 64
-fi
 
 USERNAME="$1"
 PASSWORD="$2"
 NAME="$3"
 SCHEMA="$4"
 IP="$5"
+CID="$6"
 
 echo
 echo "USERNAME: $USERNAME"
@@ -22,6 +15,7 @@ echo "PASSWORD: $PASSWORD"
 echo "NAME: $NAME"
 echo "SCHEMA: $SCHEMA"
 echo "IP: $IP"
+echo "CID: $CID"
 echo
 
 export PGPASSWORD=password
@@ -30,12 +24,16 @@ echo "---"
 echo "INIT START"
 #export PGPASSWORD=$PASSWORD
 #psql -h$IP -U $USERNAME -d $NAME -f $SCHEMA
-psql -h$IP -U postgres -c "CREATE DATABASE test"
-psql -h$IP -U postgres -d test -c "CREATE TABLE sys_entity(id VARCHAR (255) NOT NULL, zone VARCHAR (255), base VARCHAR (255), name VARCHAR (255) NOT NULL, fields TEXT, PRIMARY KEY (id))"
-psql -h$IP -U postgres -d test -c "CREATE TABLE test (id VARCHAR (255) NOT NULL, test1 VARCHAR (255), test2 VARCHAR (255), seneca VARCHAR (255), PRIMARY KEY (id))"
-psql -h$IP -U postgres -d $NAME -f $SCHEMA
-psql -h$IP -U postgres -c "CREATE USER \"$USERNAME\" WITH PASSWORD '"$PASSWORD"'"
-psql -h$IP -U postgres -c "ALTER USER \"$USERNAME\" WITH SUPERUSER"
+while read chunk; do
+  chunk=$(echo "$chunk" | sed 's/"/\\\'\"'/g')
+  docker exec "$CID" bash -c "echo \"$chunk\" >> data.postgresql.sql"  
+done < "$SCHEMA"
+docker exec "$CID" bash -c "export PGPASSWORD=password && psql -h$IP -U postgres -c \"CREATE DATABASE test\""
+docker exec "$CID" bash -c "export PGPASSWORD=password && psql -h$IP -U postgres -d test -c \"CREATE TABLE sys_entity(id VARCHAR (255) NOT NULL, zone VARCHAR (255), base VARCHAR (255), name VARCHAR (255) NOT NULL, fields TEXT, PRIMARY KEY (id))\""
+docker exec "$CID" bash -c "export PGPASSWORD=password && psql -h$IP -U postgres -d test -c \"CREATE TABLE test (id VARCHAR (255) NOT NULL, test1 VARCHAR (255), test2 VARCHAR (255), seneca VARCHAR (255), PRIMARY KEY (id))\""
+docker exec "$CID" bash -c "export PGPASSWORD=password && psql -h$IP -U postgres -d $NAME -f data.postgresql.sql"
+docker exec "$CID" bash -c "export PGPASSWORD=password && psql -h$IP -U postgres -c \"CREATE USER \\\""$USERNAME"\\\" WITH PASSWORD '"$PASSWORD"'\""
+docker exec "$CID" bash -c "export PGPASSWORD=password && psql -h$IP -U postgres -c \"ALTER USER \\\""$USERNAME"\\\" WITH SUPERUSER\""
 echo "INIT COMPLETE"
 echo "---"
 #echo "USE [CTRL]+[D] to leave" # used to login
